a22727d669929715e4a2e67cca81e707557b2836
longhorn2000 <80234800+longhorn2000@users.noreply.github.com>
2021-08-28 13:49:06 -0500

    updates

--- app_.py
+++ app_.py
@@ -69,5 +69,17 @@ def index():
     
     return render_template("indexmer.html")
 
-if __name__ == "__main__":
-	app.run(debug=True)
+    @app.route('/video_feed')
+def video_feed():
+    #Video streaming route. Put this in the src attribute of an img tag
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/')
+def index():
+    """Video streaming home page."""
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
